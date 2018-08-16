7d3764a5be7139d7ec0a50816d93cb0e4a46a2e3
FloWuenne <flowuenne@gmail.com>
2018-08-16 11:06:11 -0400

    First test function for testing commit to github.

---
+++ R/plot_phenotye_distributions_UKBB.R
@@ -0,0 +1,8 @@
+#' A function to plot distributions of phenotypes from UKBB
+#'
+#' This function will produce some quality plots to assess distribution of phenotypes for a given UKBB file.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords UKBB, phenotypes
+#' @export
+#' @examples
+#' plot_UKBB_pheno()
